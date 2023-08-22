d37d819eb3d66dc4861a6332c8eadfe7972f8e09
pritam2003 <85568503+pritam2003@users.noreply.github.com>
2023-08-22 14:03:09 -0700

    Create Showcase.tsx

---
+++ Showcase.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import './Showcase.css';
+
+const Showcase: React.FC = () => {
+
+  useEffect(() => {
+    const tabItems = document.querySelectorAll('.tab-item');
+    const tabContentItems = document.querySelectorAll('.tab-content-item');
+    
+    // ... (Rest of the JavaScript from nc_3.js)
+    // Convert the JavaScript to work within React's useEffect
+
+  }, []);
+
+  return (
+    <>
+      {/* ... (HTML content from nc_1.html) */}
+    </>
+  );
+}
+
+export default Showcase;
